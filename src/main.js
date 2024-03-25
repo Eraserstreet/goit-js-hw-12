e6bee54636f3aa211119b343ab531f15bc2b2bda
Eraserstreet <148534901+Eraserstreet@users.noreply.github.com>
2024-03-25 11:29:31 +0200

    Home-JS-11-0.6
    виправлення по зауваження ментора

--- src/main.js
+++ src/main.js
@@ -7,37 +7,51 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { getImage } from './js/pixabay-api';
 import { imageTemplate } from './js/render-functions';
 
+// помилка
+function showErrorToast() {
+  iziToast.error({
+    maxWidth: '432px',
+    height: '48px',
+    color: 'red',
+    position: 'topRight',
+    message:
+      'Sorry, there are no images matching your search query. Please try again!',
+  });
+}
+
+// зображення
+function displayImages(data) {
+  const markup = imageTemplate(data.hits);
+  imgGallery.innerHTML = markup;
+  const galleryCfg = { captionsData: 'alt' };
+  let lightbox = new SimpleLightbox('.gallery a', galleryCfg);
+  lightbox.on('show.simplelightbox', function () {});
+  lightbox.refresh();
+}
+
+// DOM
 export const imgGallery = document.querySelector('.gallery');
 export const formEl = document.querySelector('.form');
 
+// Слухач подій для форми
 formEl.addEventListener('submit', event => {
   event.preventDefault();
   const inputValue = event.currentTarget.elements.image.value.trim();
   imgGallery.innerHTML = '<div class="loader"></div>';
+
+  // Зображення і обробка
   getImage(inputValue)
     .then(data => {
-      const markup = imageTemplate(data.hits);
-      imgGallery.innerHTML = markup;
-      const galleryCfg = {
-        captionsData: 'alt',
-      };
-      let lightbox = new SimpleLightbox('.gallery a', galleryCfg);
-      lightbox.on('show.simplelightbox', function () {});
-      lightbox.refresh();
       if (data.hits.length === 0) {
-        return error;
+        showErrorToast();
+      } else {
+        displayImages(data);
       }
     })
     .catch(error => {
-      iziToast.error({
-        maxWidth: '432px',
-        height: '48px',
-        color: 'red',
-        position: 'topRight',
-        message: 'Error occurred while loading images. Please try again later.',
-      });
+      showErrorToast();
     })
     .finally(() => {
-      return formEl.reset();
+      formEl.reset();
     });
 });
